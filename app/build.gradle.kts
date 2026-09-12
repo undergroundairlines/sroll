ccf7f28b1c8a6141579b1ef39fb7c352e4923750
undergroundairlines <fanfred76acc@gmail.com>
2026-09-13 01:07:41 +1200

    feat: add screen time impact comparison

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ extensions.configure<ApplicationExtension> {
     // ... Application Version ...
     val majorUpdateVersion = 0
     val minorUpdateVersion = 3
-    val patchVersion = 0
+    val patchVersion = 1
 
     val mVersionCode = majorUpdateVersion.times(10_000)
         .plus(minorUpdateVersion.times(100))
